de2bd6aacad07147b2d9fa2c0c0fd5dace488e98
Sigrid Aasma <sigridaasma@gmail.com>
2018-12-13 01:24:27 +0200

    rendering fetched data

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -7,26 +7,35 @@ import './Blog.css';
 import axios from 'axios'
 
 class Blog extends Component {
+
+    state = {
+        posts: []
+    };
+
     componentDidMount() {
         axios.get('https://jsonplaceholder.typicode.com/posts')
             .then(response => {
+                this.setState({posts: response.data});
                 console.log(response);
             });
     }
 
     render () {
+        const posts = this.state.posts.map(post => {
+                return <Post key={post.id} title={post.title}/>
+            }
+        );
+
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost/>
                 </section>
                 <section>
-                    <NewPost />
+                    <NewPost/>
                 </section>
             </div>
         );
