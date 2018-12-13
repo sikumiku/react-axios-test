ad0bb4359b79b98eb4373ba9b88f9c7a4cfd64b1
Sigrid Aasma <sigridaasma@gmail.com>
2018-12-13 02:08:20 +0200

    setting global configuration

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import axios from 'axios';
 
+axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
+axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
+
 // gives opportunity to manipulate with requests and response (handle errors or add headers etc)
 axios.interceptors.request.use(request => {
     return request;
