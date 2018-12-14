30787a1e54dfa86e219d4bd034ca83e77383a468
Sigrid Aasma <sigridaasma@gmail.com>
2018-12-15 01:36:02 +0200

    lazy loading

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 import {BrowserRouter} from 'react-router-dom';
-
+// doing dynamic import with latest react
+// const Posts = React.lazy(() => import('./containers/Blog/Blog'));
+// Suspense needs to be imported to use it in Route like this:
+/*
+    <Route path="/posts" render={() => {
+        <Suspense fallback{<div>Loading..</div>}>
+            <Posts/>
+        </Suspense>
+        )}
+    />
+* */
 import Blog from './containers/Blog/Blog';
 
 class App extends Component {
   render() {
     return (
+        //possible to specify a different route than domain here with server: <BrowserRouter basename="/my-app">
         <BrowserRouter>
           <div className="App">
             <Blog />
